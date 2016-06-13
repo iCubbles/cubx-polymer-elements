30a824d9d75313e6f3370d40708f9ff3009edc1c
Edwin Gamboa <Edwin.Gamboa@incowia.com>
2016-06-13 09:55:56 +0200

    [PLAT-532] Add support for submenus

--- webpackages/com.incowia.cubx-paper-menu/cubx-paper-menu/cubx-paper-menu.js
+++ webpackages/com.incowia.cubx-paper-menu/cubx-paper-menu/cubx-paper-menu.js
@@ -12,7 +12,7 @@
   CubxPolymer({
     is: 'cubx-paper-menu',
 
-    _countItems: 0,
+    _countIds: 0,
 
     /**
      * Manipulate an element’s local DOM when the element is created.
@@ -24,7 +24,7 @@
      * Manipulate an element’s local DOM when the element is created and initialized.
      */
     ready: function () {
-      this.setItems([]);
+      this.setMenuItems([]);
     },
 
     /**
@@ -37,10 +37,17 @@
      * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
      */
     cubxReady: function () {
+      this.addEventListener('iron-select', function () {
+        this.setSelected(this.$$('paper-menu').selected);
+        this.setSelectedItem(this.$$('paper-menu').selectedItem);
+        this.setSelectedItems(this.$$('paper-menu').selectedItems);
+        this.setSelectedValues(this.$$('paper-menu').selectedValues);
+      });
     },
 
     /**
-     *  Observe the Cubbles-Component-Model: If value for slot 'id' has changed, the paper-menu element's property with the same name is updated
+     *  Observe the Cubbles-Component-Model: If value for slot 'id' has changed, the paper-menu element's property with
+     *  the same name is updated
      */
     modelIdChanged: function (id) {
       this.$$('paper-menu').id = id;
@@ -143,34 +150,33 @@
     },
 
     /**
-     *  Observe the Cubbles-Component-Model: If value for slot 'items' has changed, the items are added to the
+     *  Observe the Cubbles-Component-Model: If value for slot 'menuItems' has changed, the menuItems are added to the
      *  paper-menu element's
      */
-    modelItemsChanged: function (items) {
-      this._removeAllItems();
-      for (var i = 0; i < this.getItems().length; i++) {
-        var polyElement = this._createPaperItem(this.getItems()[i]);
-        if (!this.getItems()[i].id) {
-          this.getItems()[i].id = polyElement.id;
+    modelMenuItemsChanged: function (menuItems) {
+      this._removeChildrenByTagName('paper-item');
+      for (var i = 0; i < this.getMenuItems().length; i++) {
+        var polyElement = this._createMenuItem(this.getMenuItems()[i]);
+        if (!this.getMenuItems()[i].id) {
+          this.getMenuItems()[i].id = polyElement.id;
         }
         this._appendElementToTheMenu(polyElement);
-        this._countItems++;
       }
     },
 
     /**
      * Observe the Cubbles-Component-Model: If value for slot 'itemToAdd', add the itemToAdd to the google-map
      */
-    modelItemToAddChanged: function (itemToAdd) {
-      this._addItem(itemToAdd);
+    modelMenuItemToAddChanged: function (itemToAdd) {
+      this._addMenuItem(itemToAdd);
     },
 
     /**
      * Observe the Cubbles-Component-Model: If value for slot 'itemToRemove', remove the itemToRemove from
      * the google-map
      */
-    modelItemToRemoveChanged: function (itemToRemove) {
-      this._removeItem(itemToRemove);
+    modelMenuItemToRemoveChanged: function (itemToRemove) {
+      this._removeElement(itemToRemove);
     },
 
     /**
@@ -178,10 +184,8 @@
      * @param {object} item - Object to be used as base to create the paper-item element to be added
      * @return {Element} - Added item
      */
-    _addItem: function (item) {
-      var newPaperItem = this._addPaperItem(item, this._createPaperItem(item));
-      this._countItems++;
-      return newPaperItem;
+    _addMenuItem: function (item) {
+      return this._addElement(item, this._createMenuItem(item));
     },
 
     /**
@@ -191,13 +195,27 @@
      * @return {Object} Added item
      * @private
      */
-    _addPaperItem: function (baseObject, element) {
+    _addElement: function (baseObject, element) {
       if (!baseObject.id) {
         baseObject.id = element.id;
       }
-      this.getItems().push(baseObject);
+      this.getMenuItems().push(baseObject);
       this._appendElementToTheMenu(element);
       return baseObject;
+    },
+
+    /**
+     * Create a paper-item or a paper-submenu depending on the passed argument
+     * @param {object} baseObject - Object to be used to create the polymer element
+     * @return {Element} - Created element
+     * @private
+     */
+    _createMenuItem: function (baseObject) {
+      if (baseObject.menuItems) {
+        return this._createSubmenuItem(baseObject);
+      } else {
+        return this._createPaperItem(baseObject);
+      }
     },
 
     /**
@@ -211,11 +229,45 @@
     _createPaperItem: function (baseObject) {
       var validProperties = ['active', 'ariaActiveAttribute', 'disabled', 'focused', 'stopKeyboardEventPropagation',
         'toggles', 'value', 'id', 'textContent'];
-      if (!baseObject.id) {
-        baseObject.id = this.getId() + '_' + 'paper-item' + '_' + this._countItems;
-        this.setLastId(baseObject.id);
+      return this._createPolymerElement(baseObject, 'paper-item', validProperties);
+    },
+
+    /**
+     * Create a paper-submenu element using an object 'baseObject', that can have the following properties: 'disabled',
+     * 'focused', 'opened', 'id', 'triggerItem' and 'menuItems'
+     * @param {object} baseObject - Object to be used to create the polymer element
+     * @return {Element} - Created element
+     * @private
+     */
+    _createSubmenuItem: function (baseObject) {
+      var validProperties = ['disabled', 'focused', 'opened', 'id', 'menuItems', 'triggerItem'];
+      var subMenuElement = this._createPolymerElement(baseObject, 'paper-submenu', validProperties);
+      var triggerItem = this._createMenuItem(baseObject.triggerItem);
+      triggerItem.className = 'menu-trigger';
+      Polymer.dom(subMenuElement).appendChild(triggerItem);
+      var subMenuContent = this._createPolymerElement({className: 'menu-content'}, 'paper-menu', ['className']);
+      for (var i = 0; i < baseObject.menuItems.length; i++) {
+        Polymer.dom(subMenuContent).appendChild(this._createMenuItem(baseObject.menuItems[i]));
       }
-      var element = document.createElement('paper-item');
+      Polymer.dom(subMenuElement).appendChild(subMenuContent);
+      return subMenuElement;
+    },
+
+    /**
+     * Create a marker element using an object 'baseObject', that can have the properties contain in 'validProperties'
+     * @param {object} baseObject - Object to be used to create the polymer element
+     * @param {string} tagName - Tag name of the polymer element to be created
+     * @param {string[]} validProperties - Valid properties for the polymer element
+     * @return {Element} - Created element
+     * @private
+     */
+    _createPolymerElement: function (baseObject, tagName, validProperties) {
+      if (!baseObject.id) {
+        baseObject.id = this.getId() + '_' + tagName + '_' + this._countIds;
+        this.setLastId(baseObject.id);
+        this._countIds ++;
+      }
+      var element = document.createElement(tagName);
       validProperties.forEach(function (key) {
         if (baseObject[key]) {
           element[key] = baseObject[key];
@@ -235,10 +287,11 @@
 
     /**
      * Remove the children whose element name is 'tagName' from the paper-menu
+     * @param {string} tagName - Name of the element's tag
      * @private
      */
-    _removeAllItems: function () {
-      var elements = document.querySelectorAll('paper-item');
+    _removeChildrenByTagName: function (tagName) {
+      var elements = document.querySelectorAll(tagName);
       for (var i = 0; i < elements.length; i++) {
         this._removeFromMenu(elements[i]);
       }
@@ -256,19 +309,35 @@
 
     /**
      * Remove the given object from the menu and from the items array
-     * @param {object} baseObject - Object that was used to create the Element to be removed
+     * @param {object} element - Element to be removed, should contain at least its id as property
      * @private
      */
-    _removeItem: function (baseObject) {
+    _removeElement: function (element) {
+      if (element.id) {
+        for (var i = 0; i < this.getMenuItems().length; i++) {
+          if (this.getMenuItems()[i].id && this.getMenuItems()[i].id === element.id) {
+            if (this._removeElementFromAMenu(element, this.getMenuItems())) return;
+          } else if (this.getMenuItems()[i].menuItems) {
+            if (this._removeElementFromAMenu(element, this.getMenuItems()[i].menuItems)) return;
+          }
+        }
+        console.error('The element with id \'' + element.id + '\' was not found');
+      } else {
+        console.error('The element should have the property \'id\' to be removed');
+      }
+    },
+
+    _removeElementFromAMenu: function (baseObject, baseObjectList) {
       var element;
-      for (var i = 0; i < this.getItems().length; i++) {
-        if (this.getItems()[i].id && this.getItems()[i].id === baseObject.id) {
+      for (var i = 0; i < baseObjectList.length; i++) {
+        if (baseObjectList[i].id && baseObjectList[i].id === baseObject.id) {
           element = document.querySelector('#' + baseObject.id);
           this._removeFromMenu(element);
-          this.getItems().splice(i, 1);
-          return;
+          baseObjectList.splice(i, 1);
+          return true;
         }
       }
+      return false;
     }
   });
 }());
